e81655152a7a2f12109b0668349e3bb791771b30
Sven <sven@home.lu>
2024-06-30 21:06:30 +0200

    adding user provider

---
+++ lib/data/providers/user_provider.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:buffalo_bar/data/models/user.dart';
+
+class UserProvider with ChangeNotifier {
+  User? _user;
+  bool _isLoading = false;
+
+  User? get user => _user;
+  bool get isLoading => _isLoading;
+
+  void setLoading(bool value) {
+    _isLoading = value;
+    notifyListeners();
+  }
+
+  void setUser(User user) {
+    _user = user;
+    notifyListeners();
+  }
+
+  void unsetUser() {
+    _user = null;
+    notifyListeners();
+  }
+
+  @override
+  String toString() {
+    if (_user != null) {
+      return 'Logged in User: ${_user!.username} with ID: ${_user!.id}';
+    } else {
+      return 'No user is logged in';
+    }
+  }
+}
