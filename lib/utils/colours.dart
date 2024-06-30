888fc4b097acb751aa7c9394b25c83a2698dca2b
Sven <sven@home.lu>
2024-06-30 21:11:26 +0200

    adding base buffalo colours

---
+++ lib/utils/colours.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+const Color buffaloDarkRed = Color(0x00570000);
+const Color buffaloRed = Color(0x00B19F2E);
+const Color buffaloOrange = Color(0x00FFB140);
+const Color buffaloYellow = Color(0x00F7F06D);
