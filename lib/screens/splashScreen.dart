3eee30a6d4188605a8bd2e9f400aaae86d8d2a6c
Sven <sven@home.lu>
2024-06-30 21:28:21 +0200

    login and splashscreen

---
+++ lib/screens/splashScreen.dart
@@ -0,0 +1,22 @@
+import 'package:buffalo_bar/widgets/loadingIndicatorWithText.dart';
+import 'package:flutter/material.dart';
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Expanded(
+          child: LoadingIndicatorWithText(text: 'Loading Buffalo'),
+        ),
+      ),
+    );
+  }
+}
