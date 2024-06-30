5ddb56bc24bf891ba49d7e1cbdfde917e2ec4ef4
Sven <sven@home.lu>
2024-06-30 21:07:05 +0200

    adding initial reotuer

---
+++ lib/router.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+final GoRouter router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+        path: '/',
+        builder: (context, state) {
+         return const Placeholder();
+        })
+  ]
+);
