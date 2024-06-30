8a5ba208c08ec6324689bb652a7e7f9169d5d29c
Sven <sven@home.lu>
2024-06-30 21:28:29 +0200

    adding routes

--- lib/router.dart
+++ lib/router.dart
@@ -1,12 +1,21 @@
+import 'package:buffalo_bar/data/providers/user_provider.dart';
+import 'package:buffalo_bar/screens/splashScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 
-final GoRouter router = GoRouter(
-  routes: <RouteBase>[
-    GoRoute(
-        path: '/',
-        builder: (context, state) {
-         return const Placeholder();
-        })
-  ]
-);
+final GoRouter router = GoRouter(routes: <RouteBase>[
+  GoRoute(
+      path: '/',
+      builder: (context, state) => const Splashscreen(),
+      redirect: (context, state) {
+        final userProvider = Provider.of<UserProvider>(context, listen: false);
+        if (userProvider.isLoggedIn) {
+          return '/home';
+        } else {
+          return '/login';
+        }
+      }),
+  GoRoute(path: '/home', builder: (context, state) => const Placeholder()),
+  GoRoute(path: '/login', builder: (context, state) => const Placeholder())
+]);
