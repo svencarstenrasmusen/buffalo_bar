9231417414428ed6cdd38ab69233aeca0d27ccd7
Sven <sven@home.lu>
2024-07-03 22:36:34 +0200

    using new widgets

--- lib/screens/loginScreen.dart
+++ lib/screens/loginScreen.dart
@@ -1,5 +1,7 @@
 import 'package:buffalo_bar/utils/colours.dart';
 import 'package:buffalo_bar/widgets/buffalo_widget.dart';
+import 'package:buffalo_bar/widgets/buttons/buffalo_button.dart';
+import 'package:buffalo_bar/widgets/dialogs/not_implement.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -12,9 +14,18 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: buffaloYellow,
-      body: BuffaloWidget(),
-    );
+    return Scaffold(
+        backgroundColor: buffaloYellow,
+        body: Column(
+          children: [
+            const BuffaloWidget(),
+            BuffaloButton(text: 'Login', onTap: _login)
+          ],
+        ));
+  }
+
+  void _login() {
+    showDialog(
+        context: context, builder: (context) => const NotImplementDialog());
   }
 }
