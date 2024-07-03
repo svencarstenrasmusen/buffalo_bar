10c8e1b0c76504363757b5109d5f13a3196446df
Sven <sven@home.lu>
2024-07-03 22:36:05 +0200

    adding new button

---
+++ lib/widgets/buttons/buffalo_button.dart
@@ -0,0 +1,14 @@
+import 'package:buffalo_bar/utils/colours.dart';
+import 'package:flutter/material.dart';
+
+class BuffaloButton extends StatelessWidget {
+  const BuffaloButton({super.key, required this.text, this.onTap});
+
+  final String text;
+  final void Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(onPressed: onTap, child: Text(text));
+  }
+}
