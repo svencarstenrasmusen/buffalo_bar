be3989529ae8e3ca652bd0c11e3b52d4c6e73748
Sven <sven@home.lu>
2024-07-03 22:17:50 +0200

    using BuffaloWidget

--- lib/screens/loginScreen.dart
+++ lib/screens/loginScreen.dart
@@ -1,4 +1,5 @@
 import 'package:buffalo_bar/utils/colours.dart';
+import 'package:buffalo_bar/widgets/buffalo_widget.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -13,9 +14,7 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return const Scaffold(
       backgroundColor: buffaloYellow,
-      body: Center(
-        child: Text('Login'),
-      ),
+      body: BuffaloWidget(),
     );
   }
 }
