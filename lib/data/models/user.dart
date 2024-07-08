e5322675c8e87047665d659610d6719a02137984
Sven <sven@home.lu>
2024-07-08 23:30:45 +0200

    adding email

--- lib/data/models/user.dart
+++ lib/data/models/user.dart
@@ -1,14 +1,16 @@
 class User {
   final String id;
   final String username;
+  final String? email;
 
-  User({required this.id, required this.username});
+  User({required this.id, required this.username, this.email});
 
   factory User.fromJson(Map<String, dynamic> json) {
-    return User(id: json['id'], username: json['username']);
+    return User(
+        id: json['id'], username: json['username'], email: json['email']);
   }
 
   Map<String, dynamic> toJson() {
-    return {'id': id, 'username': username};
+    return {'id': id, 'username': username, 'email': email};
   }
 }
