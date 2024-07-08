8f8f4443a0d164da17d35e704eba21209aa48e5b
Sven <sven@home.lu>
2024-07-09 00:20:23 +0200

    fetching user then logging in

---
+++ lib/config.dart
@@ -0,0 +1,2 @@
+const String baseBuffaloApi = 'http://localhost:3202';
+const String mockDataPath = 'mock_data';
--- lib/screens/loginScreen.dart
+++ lib/screens/loginScreen.dart
@@ -1,8 +1,12 @@
+import 'package:buffalo_bar/data/providers/user_provider.dart';
+import 'package:buffalo_bar/data/services/user_service.dart';
 import 'package:buffalo_bar/utils/colours.dart';
 import 'package:buffalo_bar/widgets/buffalo_widget.dart';
 import 'package:buffalo_bar/widgets/buttons/buffalo_button.dart';
 import 'package:buffalo_bar/widgets/dialogs/not_implement.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -12,23 +16,36 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final UserService userService = UserService();
+
   @override
   Widget build(BuildContext context) {
+    final userProvider = Provider.of<UserProvider>(context);
     double screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
       backgroundColor: buffaloYellow,
       body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            ConstrainedBox(
-                constraints: BoxConstraints(maxHeight: screenHeight / 2),
-                child: const BuffaloWidget()),
-            BuffaloButton(text: 'Login', onTap: _login)
-          ],
-        ),
+        child: userProvider.isLoading
+            ? const CircularProgressIndicator()
+            : Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ConstrainedBox(
+                      constraints: BoxConstraints(maxHeight: screenHeight / 2),
+                      child: const BuffaloWidget()),
+                  BuffaloButton(
+                      text: 'Login',
+                      onTap: () {
+                        userProvider.loginAndSetUser(userService).then((_) {
+                          if (userProvider.isLoggedIn) {
+                            context.go('/dashboard');
+                          }
+                        });
+                      })
+                ],
+              ),
       ),
     );
   }
