0fcef673329d93236d870002d6a68fa3aa02b57a
Sven <sven@home.lu>
2024-07-08 23:33:13 +0200

    adding createdAt

--- lib/data/models/user.dart
+++ lib/data/models/user.dart
@@ -1,13 +1,21 @@
 class User {
   final String id;
   final String username;
+  final String createdAt;
   final String? email;
 
-  User({required this.id, required this.username, this.email});
+  User(
+      {required this.id,
+      required this.username,
+      required this.createdAt,
+      this.email});
 
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
-        id: json['id'], username: json['username'], email: json['email']);
+        id: json['id'],
+        username: json['username'],
+        createdAt: json['createdAt'],
+        email: json['email']);
   }
 
   Map<String, dynamic> toJson() {
