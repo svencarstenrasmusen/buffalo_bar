19709080459ef1e54632d4492b91a6365a685572
Sven <sven@home.lu>
2024-08-17 18:44:12 +0200

    fixing config

--- lib/config.dart
+++ lib/config.dart
@@ -6,5 +6,5 @@ const String API_URL = 'http://localhost:$API_PORT';
 
 const OIDC_REDIRECT_URL = 'http%3A%2F%2Flocalhost%3A7070%2F%23%2Foidc';
 const OIDC_LOGIN_URL =
-    'http://localhost:8080/realms/buffalo/protocol/openid-connect/auth';
+    'http://localhost:8080/realms/Buffalo/protocol/openid-connect/auth';
 const OIDC_CLIENT_ID = 'buffalo';
